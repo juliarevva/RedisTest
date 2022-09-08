5e97dd14a406c47865ee09e24ca9f7af3bedb1de
Julia Revva <revva_87@mail.ru>
2022-09-08 14:14:34 +0300

    Выполнение задания 4.10_1

---
+++ src/Arithmetic.java
@@ -0,0 +1,26 @@
+public class Arithmetic {
+    private int num1;
+    private int num2;
+
+    public Arithmetic (int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int sum() {
+        return num1 + num2;
+    }
+
+    public int mult() {
+        return num1 * num2;
+    }
+
+    public int max () {
+        return (num1 > num2 ? num1 : num2);
+    }
+
+    public int min () {
+        return (num1 < num2 ? num1 : num2);
+    }
+
+}
