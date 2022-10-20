38451d0d7a522fa26734b0f85d6fa179bb958277
Julia Revva <revva_87@mail.ru>
2022-10-20 17:29:00 +0300

    Добавление Arithmetic

--- ObjectsAndMethods/src/Main.java
+++ ObjectsAndMethods/src/Main.java
@@ -4,5 +4,18 @@ public class Main {
         Basket basket = new Basket();
         basket.add("Milk", 40, 1);
         basket.print("Your basket");
+
+        Arithmetic arithmetic = new Arithmetic(3,4);
+
+        arithmetic.sum();
+        arithmetic.mult();
+        arithmetic.max();
+        arithmetic.min();
+
+        System.out.println(arithmetic.sum());
+        System.out.println(arithmetic.mult());
+        System.out.println(arithmetic.max());
+        System.out.println(arithmetic.min());
     }
+
 }
