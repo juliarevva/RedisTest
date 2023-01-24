7091cbc93887fdf40258feca55e4a998654facb1
Julia Revva <revva_87@mail.ru>
2023-01-24 19:53:09 +0300

    Homework 7.9.2_v1

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,13 +4,19 @@ public class Main {
 //        container.addCount(5672);
 //        System.out.println(container.getCount());
 
-            for (char a = 'А'; a <= 'я'; a++) {
+        for (char a = 'А'; a <= 'Я'; a++) {
                 System.out.println((int) a + "-" + a);
         }
-            char a = 'Ё';
-            char b = 'ё';
+        System.out.print("\n");
+
+        for (char a = 'а'; a <= 'я'; a++) {
+            System.out.println((int) a + "-" + a);
+        }
+        System.out.print("\n");
+
+        char a = 'Ё';
+        char b = 'ё';
         System.out.println((int) a + "-" + a +
                 "\n" + (int) b + "-" + b);
-
     }
 }
