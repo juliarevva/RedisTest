6faed018c0142cdc5502cff931089d07bf5c4a0d
Julia Revva <revva_87@mail.ru>
2023-06-06 20:25:23 +0200

    Homework_15.14_v0

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,24 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+import com.skillbox.airport.Terminal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport dixon = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(dixon));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        Date a = new Date(System.currentTimeMillis() + 7200000);
+
+        List<Terminal> terminal =  airport.getTerminals();
+        return terminal.stream().flatMap(flight -> flight.getFlights().stream()).
+                collect(Collectors.toList()).stream().filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE)).
+                filter(flight -> flight.getDate().before(a) && flight.getDate().after(new Date(System.currentTimeMillis()))).
+                collect(Collectors.toList());
     }
 
 }
