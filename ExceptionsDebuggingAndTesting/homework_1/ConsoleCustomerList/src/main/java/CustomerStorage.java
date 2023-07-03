d7019bbb512c2dc9107d067994dcc22b338802f1
Julia Revva <revva_87@mail.ru>
2023-07-03 12:47:10 +0300

    Homework_16.11.1_v0

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,14 +1,29 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerStorage {
+    private static Logger logger = LogManager.getLogger(CustomerStorage.class);
     private final Map<String, Customer> storage;
 
     public CustomerStorage() {
         storage = new HashMap<>();
     }
 
+    public boolean isPhone(String phone) {
+        String regexPhone = "[+]7[0-9]{10}";
+        return phone.matches(regexPhone);
+    }
+
+    public boolean isEmail(String phone) {
+        String regexPhone = "[a-zA-Z0-9-_.]+@[a-z]+[.][a-z]+";
+        return phone.matches(regexPhone);
+    }
+
     public void addCustomer(String data) {
+
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
@@ -16,6 +31,20 @@ public class CustomerStorage {
 
         String[] components = data.split("\\s+");
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+
+        if (components.length != 4) {
+            logger.log(Level.ERROR, "Неверное количество элементов");
+            throw new IllegalArgumentException("Введите: add Имя(Василий) Фамилию(Петров) " +
+                    "Эл.почту(vasily.petrov@gmail.com) Телефон(+79215637722)");
+        }
+        if (!isEmail(components[INDEX_EMAIL])) {
+            logger.log(Level.ERROR, "Неверный формат Эл.почты");
+            throw new IllegalArgumentException("Формат Эл.почты: vasily.petrov@gmail.com");
+        }
+        if (!isPhone(components[INDEX_PHONE])) {
+            logger.log(Level.ERROR, "Неверный формат номера телефона");
+            throw new IllegalArgumentException("Формат номера: +79175555555");
+        }
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,11 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class);
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -17,18 +22,28 @@ public class Main {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            try {
+                if (tokens[0].equals("add")) {
+                    logger.log(Level.INFO, "Запрос на добавление контакта");
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    logger.log(Level.INFO, "Запрос на получение списка контактов");
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    logger.log(Level.INFO, "Запрос на удаление контакта");
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    logger.log(Level.INFO, "Запрос количества контактов");
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    logger.log(Level.INFO, "Запрос допустимых команд");
+                    System.out.println(helpText);
+                } else {
+                    logger.log(Level.ERROR, "Ввод неизвестной команды");
+                    System.out.println(COMMAND_ERROR);
+                }
+            } catch (Exception ex) {
+                System.err.println(ex);
             }
         }
     }
